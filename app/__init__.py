2fcc37bab5b9a12bf39bb4d01db3ddd253bd752a
Shadain Gooden <shadain.gooden@mymona.uwi.edu>
2017-05-15 22:08:45 +0000

    Updated heroku database

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,12 @@ app.config['SECRET_KEY'] = " i have made a quite random key"
 app.config['TOKEN_SECRET'] = " now i have made a super secret token string"
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://wisher:pa$$word123@localhost/wishlist"
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://wisher:pa$$word123@localhost/wishlist"
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://hqaxtohhpsvuyj:4b7f4957ddde9dccbf7f739f2496d5a9d5a66c5535aab2a981a0480bb657472e@ec2-54-163-252-55.compute-1.amazonaws.com:5432/d21p31bgba74om'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 # DATABASE_URL='postgres://hqaxtohhpsvuyj:4b7f4957ddde9dccbf7f739f2496d5a9d5a66c5535aab2a981a0480bb657472e@ec2-54-163-252-55.compute-1.amazonaws.com:5432/d21p31bgba74om'
-# HEROKU_POSTGRESQL_CHARCOAL_URL='postgres://nzeedrfyktybgw:401b9e8328a019b0c413c786985ce3a9a9df8a7035cf9f0ff83c564098b7cab3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/dadursdb5ouqav'
+# app.config['HEROKU_POSTGRESQL_CHARCOAL_URL']='postgres://nzeedrfyktybgw:401b9e8328a019b0c413c786985ce3a9a9df8a7035cf9f0ff83c564098b7cab3@ec2-184-73-236-170.compute-1.amazonaws.com:5432/dadursdb5ouqav'
 
 db = SQLAlchemy(app)
 
